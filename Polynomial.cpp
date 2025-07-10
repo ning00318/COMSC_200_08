6acc291bb9d273c12a2586f2d2c1ef9a05ab29c7
NINA <ninatsao@ningningning.lan>
2025-07-10 15:19:14 -0700

    3rd commit

--- Polynomial.cpp
+++ Polynomial.cpp
@@ -1,1 +1,49 @@
 #include "Polynomial.h"
+#include <string>
+using namespace std;
+
+Polynomial::Polynomial()
+{
+
+}
+Polynomial::~Polynomial()
+{
+
+}
+void Polynomial::set()
+{
+
+}
+string Polynomial::get() const
+{
+
+}
+void Polynomial::enterTerms()
+{
+
+}
+void Polynomial::printPolynomial() const
+{
+
+}
+
+Polynomial Polynomial::operator+(const Polynomial&) const
+{
+
+}
+Polynomial Polynomial::operator-(const Polynomial&) const
+{
+
+}
+Polynomial &Polynomial::operator=(const Polynomial&)
+{
+
+}
+Polynomial &Polynomial::operator+=(const Polynomial&)
+{
+
+}
+Polynomial &Polynomial::operator-=(const Polynomial&)
+{
+
+}
