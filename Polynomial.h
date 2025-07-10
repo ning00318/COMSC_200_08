e817f26ece8440ca73dbdee09b87b23a3fe9d4e3
NINA <ninatsao@ningningning.lan>
2025-07-10 15:13:50 -0700

    2nd commit

---
+++ Polynomial.h
@@ -0,0 +1,33 @@
+#ifndef POLYNOMIAL_H
+#define POLYNOMIAL_H
+
+#include <string>
+#include <vector>
+
+class Polynomial
+{
+public:
+    Polynomial();
+    ~Polynomial();
+    void set();
+    std::string get() const;
+    void enterTerms();
+    void printPolynomial() const;
+
+    // Comment: Overload the addition and subtraction operators will create a new Polynomial object
+    //          and return the results (by value), which means that both objects' values will not be changed (const).
+    Polynomial operator+(const Polynomial&) const;
+    Polynomial operator-(const Polynomial&) const; 
+
+    // Comment: Overload =, +=, and -= operators do not create a new Polynomial object but modify the value of the current object based on another one
+    //          and return the new value to the current object (by reference), which means that the calling object's value will change but another one's remain unchanged.
+    Polynomial &operator=(const Polynomial&);
+    Polynomial &operator+=(const Polynomial&);
+    Polynomial &operator-=(const Polynomial&);
+
+private:
+    std::vector<int> coefficient;
+    std::vector<int> exponent;
+};
+
+#endif
