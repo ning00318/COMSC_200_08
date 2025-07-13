dd9957f26c59b382ba0b4ec295dd6cbcfa97d0a6
NINA <ninatsao@ningningning.local>
2025-07-13 16:26:08 -0700

    11th commit

--- Polynomial.cpp
+++ Polynomial.cpp
@@ -35,57 +35,106 @@ void Polynomial::printPolynomial() const
     Polynomial order;
     order.coefficients = coefficients;
     order.exponents = exponents;
-    bool ifDone = false;
-    int count2 = 0;
-    while (ifDone == false)
+    bool ifFirstTerm = true;
+
+    // Comment 4: Find out the constant value first and print it out.
+    //            If there is no constant value as the first term, continue.
+    for (int i = 0; i < order.exponents.size(); i++)
     {
-        int maxExponent = 0;
-        int maxExponentIndex = 0;
-        int count = 0;
-        for (int i = 0; i < order.exponents.size(); i++)
+        if (order.exponents[i] == 0 && order.coefficients[i] != 0)
         {
-            if (order.exponents[i] > maxExponent)
-            {
-                maxExponent = order.exponents[i];
-                maxExponentIndex = i;
-            }
-            else if (order.exponents[i] == 0)
-            {
-                if (order.coefficients[i] != 0)
-                    cout << order.coefficients[i];
-                order.exponents[i] = -1;
-            }
-            else if (order.exponents[i] == -1)
-            {
-                count++;
-                if (count == order.exponents.size())
-                    ifDone = true;
-            }
-        }
-        if (order.exponents[maxExponentIndex] > 0)
-        {
-            if (order.coefficients[maxExponentIndex] != 0)
-            {
-                cout << ((order.coefficients[maxExponentIndex] > 0) ? "+" : "-");
-                cout << order.coefficients[maxExponentIndex] << "x";
-                if (order.exponents[maxExponentIndex] != 1)
-                    cout << "^" << order.exponents[maxExponentIndex];
-            }
-            order.exponents[maxExponentIndex] = -1;
-        }
-        if (order.coefficients[maxExponentIndex] == 0)
-        {
-            count2++;
-            if (order.coefficients.size() == count2)
-                cout << "0";
+            printTerm(order.coefficients[i], order.exponents[i], ifFirstTerm);
+            ifFirstTerm = false;
+            order.exponents[i] = -1;    // Comment 5: Set it to -1 to indicate that this exponent has been taken.
+            break;
         }
     }
-    cout << endl; 
+
+    bool ifDone = false;
+    int count = 0;
+
+    // Comment 6: After checking if there is a constant value and printing
+    //            it out if exists, output the remaining terms that their
+    //            exponents are at least 1, and whose coeffients are not 0.
+    while (ifDone == false)
+    {
+        int maxExponentIndex = findMaxExponentIndex(order.exponents);
+        
+        // Comment 7: Checking if the maximum value of exponent index is -1 first, means
+        //            all exponents were taken, then end of loop, otherwise, continue.
+        if (maxExponentIndex == -1)
+        {
+            ifDone = true;
+            break;
+        }
+        
+        if (order.coefficients[maxExponentIndex] != 0)
+        {
+            printTerm(order.coefficients[maxExponentIndex], order.exponents[maxExponentIndex], ifFirstTerm);
+            ifFirstTerm = false;
+        }
+        else
+        {
+            // Comment 8: Adding a count when there is a term equal to 0, if
+            //            all the terms are zero, print out 0 and end of loop.
+            count++;
+            if (order.coefficients.size() == count)
+            {
+                cout << "0";
+                ifDone = true;
+            }
+        }
+
+        // Comment 9: Remember to assign the current max exponent to -1 to prevent from printing out the same result again.
+        order.exponents[maxExponentIndex] = -1;
+    }
+    cout << endl;
+}
+
+int Polynomial::findMaxExponentIndex(const std::vector<int>& exponents) const
+{
+    int maxExponent = -1;
+    int maxExponentIndex = -1;
+    for (int i = 0; i < exponents.size(); i++)
+    {
+        // Comment 10: If exponent > maxExponent, then assign current exponent to the maxExponent and store its index.
+        if (exponents[i] > maxExponent)
+        {
+            maxExponent = exponents[i];
+            maxExponentIndex = i;
+        }
+    }
+
+    // Comment 11: After finding out the max exponent, return its index
+    return maxExponentIndex;
+}
+
+void Polynomial::printTerm(int coefficient, int exponent, bool ifFirstTerm) const
+{
+    // Comment 12: If the printed term is not the first term, check if it's a positive or negative value and output respectively.
+    if (ifFirstTerm == false)
+        cout << ((coefficient >= 0) ? "+" : "-");
+    else if (coefficient < 0)   // Comment 13: Or if the printed term is the first term (means there is no
+        cout << "-";            //             constant value), output "-" when its coefficient is less than 0.
+
+    if (coefficient != 0)
+    {
+        // Comment 14: This is for the constant values, and we are printing out their absolute values
+        //             because we have already checked if they are positive or negative coefficients.
+        if (exponent == 0)
+            cout << abs(coefficient);
+        else
+        {
+            cout << abs(coefficient) << "x";
+            if (exponent != 1)
+                cout << "^" << exponent;
+        }
+    }
 }
 
 Polynomial Polynomial::operator+(const Polynomial& poly2) const
 {
-    Polynomial result;
+    Polynomial result;  // Create a new Polynomial object
     result.coefficients = coefficients;
     result.exponents = exponents;
     int resultSize = result.coefficients.size();
@@ -96,12 +145,15 @@ Polynomial Polynomial::operator+(const Polynomial& poly2) const
         bool ifFound = false;
         for (int j = 0; j < resultSize; j++)
         {
+            // Comment 15: If both terms' exponents are the same, add their coefficients together then store and update the result.
             if (poly2.exponents[i] == result.exponents[j])
             {
                 result.coefficients[j] += poly2.coefficients[i];
                 ifFound = true;
             }
         }
+
+        // Comment 16: Otherwise, the remaining terms of the other Polynomial will be stored in the result individually.
         if (ifFound == false)
         {
             result.coefficients.push_back(poly2.coefficients[i]);
--- Polynomial.h
+++ Polynomial.h
@@ -12,13 +12,14 @@ public:
     void enterTerms();
     void printPolynomial() const;
 
-    // Comment: Overload the addition and subtraction operators will create a new Polynomial object
+    // Comment 1: Overload the addition and subtraction operators will create a new Polynomial object
     //          and return the results (by value), which means that both objects' values will not be changed (const).
     Polynomial operator+(const Polynomial&) const;
     Polynomial operator-(const Polynomial&) const; 
 
-    // Comment: Overload =, +=, and -= operators do not create a new Polynomial object but modify the value of the current object based on another one
-    //          and return the new value to the current object (by reference), which means that the calling object's value will change but another one's remain unchanged.
+    // Comment 2: Overload =, +=, and -= operators do not create a new Polynomial object but modify the value of the
+    //            current object based on another one and return the new value to the current object (by reference),
+    //            which means that the calling object's value will change but another one's remain unchanged.
     Polynomial &operator=(const Polynomial&);
     Polynomial &operator+=(const Polynomial&);
     Polynomial &operator-=(const Polynomial&);
@@ -26,6 +27,10 @@ public:
 private:
     std::vector<int> coefficients;
     std::vector<int> exponents;
+
+    // Comment 3: Helper functions of printPolynomial()
+    int findMaxExponentIndex(const std::vector<int>& exponents) const;
+    void printTerm(int, int, bool) const;
 };
 
 #endif
